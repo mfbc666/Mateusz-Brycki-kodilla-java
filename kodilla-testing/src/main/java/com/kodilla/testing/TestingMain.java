93c58e787a116976e2989af2c50b91ee796c5168
unknown <mateusz.brycki.wro@gmail.com>
2017-08-21 19:43:02 +0200

    First Commit

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -0,0 +1,40 @@
+package com.kodilla.testing;
+
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
+
+
+public class TestingMain {
+    public static void main(String[] args) {
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+         //Zadanie zaliczeniowe 6.2
+
+        Calculator calculator = new Calculator();
+        System.out.println("");
+        System.out.println("Excercise 6.2");
+        System.out.println("");
+            if(calculator.add(5,5) == 10){
+                System.out.println("add passed");
+            } else {
+                System.out.println("Error!");
+            }
+            if(calculator.substract(5,5) == 0){
+                System.out.println("substract passed");
+            }else {
+                System.out.println("Error!");
+            }
+
+         //Zadanie zaliczeniowe 6.3
+
+    }
+}
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/ForumTestSuite.java
@@ -0,0 +1,8 @@
+package com.kodilla.testing.forum;
+
+        import com.kodilla.testing.user.SimpleUser;
+        import org.junit.Assert;
+
+
+public class ForumTestSuite {
+}
