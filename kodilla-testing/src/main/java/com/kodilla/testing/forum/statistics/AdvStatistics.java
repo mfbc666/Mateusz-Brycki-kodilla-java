391f0f3e6741aff835a315d0d514172633b50000
mfbc666 <mateusz.brycki.wro@gmail.com>
2017-09-07 09:46:55 +0200

    Commit
    Signed-off-by: mfbc666 <mateusz.brycki.wro@gmail.com>

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/AdvStatistics.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/AdvStatistics.java
@@ -2,13 +2,41 @@ package com.kodilla.testing.forum.statistics;
 
 import java.util.ArrayList;
 
-
 public class AdvStatistics {
-    int numberOfUsers;
-    int numberOfPosts;
-    int numberOfComments;
-    int avgNumberOfCommentsPerUser;
-    int avgNumberOfCommentsPerPost;
+    //dodać constructor gettery, zmienić pola na prywatne
+    public AdvStatistics(int numberOfUsers, int numberOfPosts, int numberOfComments, int avgNumberOfCommentsPerUser, int avgNumberOfCommentsPerPost) {
+        this.numberOfUsers = numberOfUsers;
+        this.numberOfPosts = numberOfPosts;
+        this.numberOfComments = numberOfComments;
+        this.avgNumberOfCommentsPerUser = avgNumberOfCommentsPerUser;
+        this.avgNumberOfCommentsPerPost = avgNumberOfCommentsPerPost;
+    }
+
+    public int getNumberOfUsers() {
+        return numberOfUsers;
+    }
+
+    public int getNumberOfPosts() {
+        return numberOfPosts;
+    }
+
+    public int getNumberOfComments() {
+        return numberOfComments;
+    }
+
+    public int getAvgNumberOfCommentsPerUser() {
+        return avgNumberOfCommentsPerUser;
+    }
+
+    public int getAvgNumberOfCommentsPerPost() {
+        return avgNumberOfCommentsPerPost;
+    }
+
+    private int numberOfUsers;
+    private int numberOfPosts;
+    private int numberOfComments;
+    private int avgNumberOfCommentsPerUser;
+    private int avgNumberOfCommentsPerPost;
 
     public void calculateAdvStatistics(Statistics statistics){
         numberOfUsers = statistics.usersNames().size();
