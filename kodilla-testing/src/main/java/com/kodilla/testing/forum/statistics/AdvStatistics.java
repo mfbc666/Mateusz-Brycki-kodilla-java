5130cf29ee84a4ea7bfb5c6a68136554ddeb6f3e
mfbc666 <mateusz.brycki.wro@gmail.com>
2017-09-08 08:01:12 +0200

    Commit
    Signed-off-by: mfbc666 <mateusz.brycki.wro@gmail.com>

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/AdvStatistics.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/AdvStatistics.java
@@ -1,15 +1,25 @@
 package com.kodilla.testing.forum.statistics;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class AdvStatistics {
     //dodać constructor gettery, zmienić pola na prywatne
-    public AdvStatistics(int numberOfUsers, int numberOfPosts, int numberOfComments, int avgNumberOfCommentsPerUser, int avgNumberOfCommentsPerPost) {
-        this.numberOfUsers = numberOfUsers;
-        this.numberOfPosts = numberOfPosts;
-        this.numberOfComments = numberOfComments;
-        this.avgNumberOfCommentsPerUser = avgNumberOfCommentsPerUser;
-        this.avgNumberOfCommentsPerPost = avgNumberOfCommentsPerPost;
+
+    private int numberOfUsers;
+    private int numberOfPosts;
+    private int numberOfComments;
+    private int avgNumberOfCommentsPerUser;
+    private int avgNumberOfCommentsPerPost;
+
+    public void calculateAdvStatistics(Statistics statistics) {
+        numberOfUsers = statistics.usersNames().size();
+        numberOfPosts = statistics.postsCount();
+        numberOfComments = statistics.commentsCount();
+        avgNumberOfCommentsPerUser = statistics.commentsCount() / statistics.usersNames().size();
+        avgNumberOfCommentsPerPost = statistics.commentsCount() / statistics.postsCount();
+    }
+    public AdvStatistics() {
     }
 
     public int getNumberOfUsers() {
@@ -32,21 +42,7 @@ public class AdvStatistics {
         return avgNumberOfCommentsPerPost;
     }
 
-    private int numberOfUsers;
-    private int numberOfPosts;
-    private int numberOfComments;
-    private int avgNumberOfCommentsPerUser;
-    private int avgNumberOfCommentsPerPost;
-
-    public void calculateAdvStatistics(Statistics statistics){
-        numberOfUsers = statistics.usersNames().size();
-        numberOfPosts = statistics.postsCount();
-        numberOfComments = statistics.commentsCount();
-        avgNumberOfCommentsPerUser = statistics.commentsCount()/statistics.usersNames().size();
-        avgNumberOfCommentsPerPost = statistics.commentsCount()/statistics.postsCount();
-    }
-
-    public void ShowStatistics(){
+    public void ShowStatistics() {
         System.out.println("Number of Users " + numberOfUsers);
         System.out.println("Number of Posts" + numberOfPosts);
         System.out.println("Number of Comments" + numberOfComments);
@@ -54,4 +50,5 @@ public class AdvStatistics {
         System.out.println("Avg number of Comments per Post" + avgNumberOfCommentsPerPost);
     }
 }
+
 
