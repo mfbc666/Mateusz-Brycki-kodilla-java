854dd145aca9d04123046e65a526def3b2d214a8
mfbc666 <mateusz.brycki.wro@gmail.com>
2017-09-08 07:58:51 +0200

    Commit
    Signed-off-by: mfbc666 <mateusz.brycki.wro@gmail.com>

--- kodilla-testing/src/test/java/com/kodilla/testing/forum/statictics/ForumStatisticsTest.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statictics/ForumStatisticsTest.java
@@ -3,23 +3,42 @@ package com.kodilla.testing.forum.statictics;
 import com.kodilla.testing.forum.statistics.AdvStatistics;
 import com.kodilla.testing.forum.statistics.Statistics;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class ForumStatisticsTest {
+
+    private Statistics statisticsMock;
+    private AdvStatistics target;
+
+    @Before
+    public void setUp(){
+        statisticsMock = mock(Statistics.class);
+        target = new AdvStatistics();
+    }
+
     @Test
     public void forumStatisticsWithMock(){
         //Given
-        Statistics statisticsMock = mock(Statistics.class);
-        AdvStatistics advStatistics =mock(AdvStatistics.class);
+        when(statisticsMock.commentsCount()).thenReturn(1000);
+        when(statisticsMock.usersNames()).thenReturn(Collections.singletonList("user"));
+        /*when(statisticsMock.postsCount()).thenReturn(7);*/
 
         //When
-
+        target.calculateAdvStatistics(statisticsMock);
 
         //Then
-        assertEquals(1000, advStatistics.getAvgNumberOfCommentsPerUser());
+        assertEquals(1000, target.getAvgNumberOfCommentsPerUser());
+        /*assertEquals(5, target.getNumberOfPosts());
+        assertEquals(7, target.getNumberOfComments());*/
+
 
     }
 
