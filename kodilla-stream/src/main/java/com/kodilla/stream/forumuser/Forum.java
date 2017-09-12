bcf059aa7ff2acd21ff522e80a588f8e82bd0f35
mfbc666 <mateusz.brycki.wro@gmail.com>
2017-09-12 06:37:59 +0200

    Commit
    Signed-off-by: mfbc666 <mateusz.brycki.wro@gmail.com>

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
@@ -0,0 +1,23 @@
+package com.kodilla.stream.forumuser;
+
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Forum {
+    private final List<ForumUser> forumUser = new ArrayList<>();
+
+    public Forum() {
+        forumUser.add(new ForumUser(1,"Dylan",'M',2009,11,12));
+        forumUser.add(new ForumUser(2,"Bob",'M',2012,2,22));
+        forumUser.add(new ForumUser(3,"Oprah",'F',2012,10,4));
+        forumUser.add(new ForumUser(4,"Spitygniew",'M',2016,5,1));
+        forumUser.add(new ForumUser(5,"Swidrygiello",'M',1309,6,12));
+        forumUser.add(new ForumUser(6,"Rus",'M',700,6,30));
+    }
+
+    public List<ForumUser> getUserList(){
+        return new  ArrayList<ForumUser>(forumUser);
+    }
+
+}
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumuser/ForumUser.java
@@ -0,0 +1,60 @@
+package com.kodilla.stream.forumuser;
+
+import java.time.LocalDate;
+
+public final class ForumUser {
+    private final int userId;
+    private final String userName;
+    private final char sex = 'M'/'F';
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    private final LocalDate dateOfBirth = LocalDate.now();
+    int year = dateOfBirth.getYear();
+    int month = dateOfBirth.getMonthValue();
+    int day = dateOfBirth.getDayOfMonth();
+
+    public ForumUser(int userId, String userName, char sex, int year, int month, int day) {
+        this.userId = userId;
+        this.userName = userName;
+        //this.sex = sex;
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+    @Override
+    public String toString(){
+        return  "Forum User " + userName + ", user ID : " + userId + ", sex: " + sex + ", date of birth " + dateOfBirth;
+    }
+}
+
+
+
+
