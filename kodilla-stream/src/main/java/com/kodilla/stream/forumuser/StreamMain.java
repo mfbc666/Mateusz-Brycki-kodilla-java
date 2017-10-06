5c66bc8b486e8f123f35cf97f1a68d5f33dfecd0
mfbc666 <mateusz.brycki.wro@gmail.com>
2017-10-06 17:53:25 +0200

    Commit
    Signed-off-by: mfbc666 <mateusz.brycki.wro@gmail.com>

--- kodilla-stream/src/main/java/com/kodilla/stream/forumuser/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumuser/StreamMain.java
@@ -2,13 +2,17 @@ package com.kodilla.stream.forumuser;
 
 public class StreamMain {
     public static void main(String[] args) {
-        Forum.getUserList().stream();
+        Forum forum = new Forum();
+        forum.getUserList().stream();
+                .filter(s -> s.)
 
+                 /*
                 .map(String::toUpperCase)
                 .filter(s -> s.length() > 11)
                 .map(s -> s.substring(0, s.indexOf(' ') + 2) + ".")
                 .filter(s -> s.substring(0, 1).equals("M"))
                 .forEach(System.out::println);
+                */
 
     }
 }
