f3a91f5cd90647800e5cab9c1d4281165637d5ba
mfbc666 <mateusz.brycki.wro@gmail.com>
2017-11-01 21:26:54 +0100

    Commit
    Signed-off-by: mfbc666 <mateusz.brycki.wro@gmail.com>

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -1,16 +1,17 @@
 package com.kodilla.exception.test;
 
-public class ExceptionHandling  {
+public class ExceptionHandling {
     public static void main(String[] args) {
         SecondChallenge secondChallenge = new SecondChallenge();
 
-        try{
-            secondChallenge.probablyIWillThrowException();
+        try {
+            secondChallenge.probablyIWillThrowException(1,2);
+        } catch (Exception e) {
+            System.out.println("catch");
+        } finally {
+            System.out.println("test have ended");
         }
     }
-
-
-
 
 
 }
