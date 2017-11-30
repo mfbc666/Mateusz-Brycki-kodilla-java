668ec82fac56eb8876b584eb2890916190a0b153
mfbc666 <mateusz.brycki.wro@gmail.com>
2017-11-30 20:14:58 +0100

    Commit
    Signed-off-by: mfbc666 <mateusz.brycki.wro@gmail.com>

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/excerciseNineTwo/Application.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/excerciseNineTwo/Application.java
@@ -5,7 +5,8 @@ public class Application {
     public static void main(String[] args) {
 
         OrderReceived orderReceived = new OrderReceived();
-        OrderRequest orderRequest = new OrderRequest()
+        OrderRequest orderRequest = orderReceived.receive();
+
 
 
     }
